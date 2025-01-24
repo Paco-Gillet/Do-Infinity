f39f0c434500ccec04cf2ad4b3f3e55d06abd763
Val <118047553+valdu34@users.noreply.github.com>
2025-01-24 21:53:09 +0100

    feat
    rajout de mélodie en tant qu'obstacle

--- scripts/scene_partition.gd
+++ scripts/scene_partition.gd
@@ -1,38 +1,42 @@
 extends Node
 
-@export var note_scene: PackedScene
+@export var melodie_scene: PackedScene
 var score
 
 func _ready():
 	new_game()
 
-func game_over():
-	$ScoreTimer.stop()
-	$NoteTimer.stop()
-	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	
-func _on_start_timer_timeout():
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$NoteTimer.stop()
+
+
+func _on_start_timer_timeout() -> void:
 	$NoteTimer.start()
 	$ScoreTimer.start()
-	
-func _on_score_timer_timeout():
+
+
+func _on_score_timer_timeout() -> void:
 	score += 1
-	
-func _on_note_timer_timeout():
-	var note = note_scene.instantiate()
+
+
+func _on_note_timer_timeout() -> void:
+	var melodie = melodie_scene.instantiate()
 	
 	var note_spawn_location = get_node("NotePath/NoteSpawnLocation")
 	note_spawn_location.progress_ratio = randf()
 	
-	var direction = note_spawn_location.rotation
+	var direction = note_spawn_location.rotation + PI / 2
 	
-	note.position = note_spawn_location.position
+	melodie.position = note_spawn_location.position
 	
 	var velocity = Vector2(200.0, 0.0)
-	note.linear_velocity = velocity.rotated(direction)
-		
-	add_child(note)
+	melodie.linear_velocity = velocity.rotated(direction)
+	
+	add_child(melodie)
