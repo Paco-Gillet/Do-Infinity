99c2955ad1cdb924f918a837bfd5fe1cf4b547cf
Timéo Jost <timeo.jost@etu.umontpellier.fr>
2025-01-24 17:48:36 +0100

    feat : main

--- scripts/scene_partition.gd
+++ scripts/scene_partition.gd
@@ -1,11 +1,38 @@
-extends Node2D
+extends Node
 
+@export var note_scene: PackedScene
+var score
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	new_game()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func game_over():
+	$ScoreTimer.stop()
+	$NoteTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	
+func _on_start_timer_timeout():
+	$NoteTimer.start()
+	$ScoreTimer.start()
+	
+func _on_score_timer_timeout():
+	score += 1
+	
+func _on_note_timer_timeout():
+	var note = note_scene.instantiate()
+	
+	var note_spawn_location = get_node("NotePath/NoteSpawnLocation")
+	note_spawn_location.progress_ratio = randf()
+	
+	var direction = note_spawn_location.rotation
+	
+	note.position = note_spawn_location.position
+	
+	var velocity = Vector2(200.0, 0.0)
+	note.linear_velocity = velocity.rotated(direction)
+		
+	add_child(note)
