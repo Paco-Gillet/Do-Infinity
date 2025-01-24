f0882f899ed10fb674bbd0971024e0c9be821832
Camilo <camilo_hd@yahoo.com>
2025-01-25 00:39:10 +0100

    fix

--- scenes/menu/menuOption.gd
+++ scenes/menu/menuOption.gd
@@ -1,12 +1,17 @@
 extends Control
 
 func _ready():
-	$TabContainer/Video/MarginContainer2/ScrollContainer/VBoxContainer/HBoxContainer/FullScreenCheckButton.connect("toggled", Callable(self, "_on_fullscreen_toggled"))
+	var fullscreen = DisplayServer.window_get_mode() == DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN
+	$TabContainer/Video/MarginContainer2/ScrollContainer/VBoxContainer/HBoxContainer/FullScreenCheckButton.button_pressed = fullscreen
+	$TabContainer/Video/MarginContainer2/ScrollContainer/VBoxContainer/HBoxContainer/FullScreenCheckButton.connect(
+		"toggled", 
+		Callable(self, "_on_full_screen_check_button_toggled")
+	)
 
 func _on_quit_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu/menuDebut.tscn")
 
-func _on_full_screen_check_button_toggled(toggled_on):
+func _on_full_screen_check_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
 	else:
