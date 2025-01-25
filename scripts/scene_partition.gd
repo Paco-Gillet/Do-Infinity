e20f98ce89452c8d708a9a793ca66c146ded70fe
Timéo Jost <timeo.jost@etu.umontpellier.fr>
2025-01-25 06:59:53 +0100

    feat : goodSpeed

--- scripts/scene_partition.gd
+++ scripts/scene_partition.gd
@@ -7,6 +7,7 @@ var score
 var high_score
 var lastLineSpawn = 0
 var repComp = 0
+var isMelodie = true
 
 
 func _ready():
@@ -24,8 +25,7 @@ func new_game():
 
 func game_over() -> void:
 	$ScoreTimer.stop()
-	$MelodieTimer.stop()
-	$NoteTimer.stop()
+	$SpawnTimer.stop()
 	if score > high_score:
 		high_score = score
 		print("Nouveau meilleur score : " + str(high_score / 10))
@@ -35,40 +35,64 @@ func game_over() -> void:
 
 
 func _on_start_timer_timeout() -> void:
-	$MelodieTimer.start()
+	$SpawnTimer.start()
 	$ScoreTimer.start()
 
 
 func _on_score_timer_timeout() -> void:
-	score += 1
+	score += 2
 	$Score.text = "Score: " + str(score/10)
 	$Score.show()
 
 
-func _on_note_timer_timeout() -> void:
-	var note = note_scene.instantiate()
+func _on_spawn_timer_timeout() -> void:
+	if !isMelodie:
+		var note = note_scene.instantiate()
 
-	var nbPointSpawn: int = randi_range(1,4) 
-	if(nbPointSpawn == lastLineSpawn && repComp>=2):
-		repComp += 1
+		var nbPointSpawn: int = randi_range(1,4) 
+		if(nbPointSpawn == lastLineSpawn && repComp>=2):
+			repComp += 1
+		else :
+			if(repComp==3):
+				while (nbPointSpawn == lastLineSpawn ):
+					nbPointSpawn = randi_range(1,4) 
+				repComp = 0
+
+		var pathPointSpawn : String =  str("NotePath/PointSpawnLocation",nbPointSpawn)
+		var spawn_location = get_node(pathPointSpawn)
+		var direction = spawn_location.rotation + PI / 2
+		var velocity = lerp(Vector2(300.0, 0.0).rotated(direction),Vector2(3000.0, 0.0).rotated(direction),0.2)
+		
+		note.position =  spawn_location.position
+		note.set_velocity(velocity)
+		add_child(note)
+		isMelodie=true
 	else :
-		if(repComp==3):
-			while (nbPointSpawn == lastLineSpawn ):
-				nbPointSpawn = randi_range(1,4) 
-			repComp = 0
-
-	var pathPointSpawn : String =  str("NotePath/PointSpawnLocation",nbPointSpawn)
-	var spawn_location = get_node(pathPointSpawn)
-	var direction = spawn_location.rotation + PI / 2
-	var velocity = lerp(Vector2(300.0, 0.0).rotated(direction),Vector2(3000.0, 0.0).rotated(direction),0.1)
+		var melodie = melodie_scene.instantiate()
 	
-	note.position =  spawn_location.position
-	note.set_velocity(velocity)
-	add_child(note)
-	if $NoteTimer.wait_time > 0.2:
-		$NoteTimer.wait_time-=0.01
-	$NoteTimer.stop()
+		var nbPointSpawn: int = randi_range(1,4) 
+		if(nbPointSpawn == lastLineSpawn && repComp>=2):
+			repComp += 1
+		else :
+			if(repComp==3):
+				while (nbPointSpawn == lastLineSpawn ):
+					nbPointSpawn = randi_range(1,4) 
+				repComp = 0
 
+		var pathPointSpawn : String =  str("NotePath/PointSpawnLocation",nbPointSpawn)
+		var spawn_location = get_node(pathPointSpawn)
+		
+		var direction = spawn_location.rotation + PI / 2
+		var velocity = lerp(Vector2(300.0, 0.0).rotated(direction),Vector2(3000.0, 0.0).rotated(direction),0.2)
+		
+			
+		melodie.position = spawn_location.position
+		melodie.set_velocity(velocity)
+		add_child(melodie)
+		isMelodie=false
+	if $SpawnTimer.wait_time > 0.1:
+		$SpawnTimer.wait_time = lerp(0.5,0.1,0.8)
+	
 func save_high_score():
 	var save_file = ConfigFile.new()
 	save_file.set_value("scores", "high_score", high_score)
@@ -96,30 +120,3 @@ func load_high_score():
 func _process(delta):
 	if Input.is_action_just_pressed("escape"):
 		game_over()
-
-
-func _on_melodie_timer_timeout() -> void:
-	var melodie = melodie_scene.instantiate()
-	
-	var nbPointSpawn: int = randi_range(1,4) 
-	if(nbPointSpawn == lastLineSpawn && repComp>=2):
-		repComp += 1
-	else :
-		if(repComp==3):
-			while (nbPointSpawn == lastLineSpawn ):
-				nbPointSpawn = randi_range(1,4) 
-			repComp = 0
-
-	var pathPointSpawn : String =  str("NotePath/PointSpawnLocation",nbPointSpawn)
-	var spawn_location = get_node(pathPointSpawn)
-	
-	var direction = spawn_location.rotation + PI / 2
-	var velocity = lerp(Vector2(300.0, 0.0).rotated(direction),Vector2(3000.0, 0.0).rotated(direction),0.1)
-	
-		
-	melodie.position = spawn_location.position
-	melodie.set_velocity(velocity)
-	add_child(melodie)
-	if $NoteTimer.wait_time > 0.4:
-		$MelodieTimer.wait_time-=0.02
-	$NoteTimer.start()
